fa37ec628929cc0cb90ecf5aa8e5869a77ae11dd
azharhacks <azhar.abeid@strathmore.edu>
2025-04-29 12:58:59 +0300

    Add files via upload
    Challenge 3-If-else decision

---
+++ Decision.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Decision {
+    Scanner scanner = new Scanner(System.in);
+    //input value
+    public int getNumber(){
+        System.out.println("Enter a number between 1 and 4: ");	
+  int number=scanner.nextInt();
+  return number;
+      }
+    
+    //outputting 
+    public void checkNumber(int number){
+        if (number == 1) {
+            System.out.println("You entered 1.");
+        } else if (number == 2) {
+            System.out.println("You entered 2.");
+        } else if (number == 3) {
+            System.out.println("You entered 3.");
+        } else if (number == 4) {
+            System.out.println("You entered 4.");
+        } else {
+            System.out.println("Invalid input. Please enter a number between 1 and 4.");
+        }
+    }
+   
+public static void main(String[] args) {
+    Decision decision = new Decision();
+   int number= decision.getNumber();
+    decision.checkNumber(number);
+}
+}
