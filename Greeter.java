a36b3718c7ad29e2e2e80fcc50cfc3428cd04c83
azharhacks <azhar.abeid@strathmore.edu>
2025-04-29 12:56:56 +0300

    Add files via upload
    Challenge 1-Calling a function

---
+++ Greeter.java
@@ -0,0 +1,8 @@
+public class Greeter {
+    void greet(){
+        System.out.println("Hello,how are you");
+    }
+    public static void main(String args[]){
+        (new Greeter()).greet();
+    }
+}
