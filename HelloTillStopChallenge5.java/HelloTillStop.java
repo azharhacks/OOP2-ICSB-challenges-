741e7488113a74b8bd71b244c0db62b46789f571
azharhacks <azharabeid5@gmail.com>
2025-06-30 12:59:24 +0300

    change folder

---
+++ HelloTillStopChallenge5.java/HelloTillStop.java
@@ -0,0 +1,18 @@
+//Challenge 5
+import java.util.Scanner;
+class HelloTillStop {
+    Scanner sc=new Scanner(System.in);
+    String choice;
+    void showHello(){
+       
+
+        System.out.println("Enter the letter 'h' to output 'hello '");
+        choice=sc.next();
+
+        while ("h".equals(choice)){
+        System.out.println("Hello there");
+        }
+        choice=sc.next();
+    }
+    
+}
---
+++ HelloTillStopChallenge5.java/WhiletestChallenge5.java
@@ -0,0 +1,8 @@
+//Challenge 5
+public class WhiletestChallenge5 {
+    public static void main (String args[]){
+        HelloTillStop h1 = new HelloTillStop();
+    
+        h1.showHello();
+}
+}
