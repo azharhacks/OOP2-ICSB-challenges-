23f0266b1ddac41dfa9fc0be8e7e86f631bd0d63
azharhacks <azharabeid5@gmail.com>
2025-07-07 11:05:58 +0300

    recursion file

---
+++ Recursion.java
@@ -0,0 +1,17 @@
+public class Recursion {
+    
+    public static void main(String[] args) {
+        int number = 5; // Example number
+        int factorial =sum(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    // Recursive method to calculate factorial
+    public static int sum(int n) {
+        if (n == 0 || n == 1) {
+            return 1; // Base case
+        } else {
+            return n + sum(n - 1); // Recursive case
+        }
+    }
+}
